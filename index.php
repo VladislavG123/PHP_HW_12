545e2357563c755d7297bd888e28751e92ab64bf
VladislavG123 <46023496+VladislavG123@users.noreply.github.com>
2020-03-08 21:32:22 +0600

    first соmmit

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 include_once 'vendor/autoload.php';
 
 use Step\Tag;
-$input = Tag::a();
-$input->setAttribute('href', 'https://google.com');
-echo $input;
+$link = Tag::a();
+$link->setAttribute('href', 'https://google.com');
+echo $link;
